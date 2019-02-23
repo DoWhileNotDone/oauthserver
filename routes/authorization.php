564c49054d55a9c0c95ec70a1ba35b2190dca174
DavegTheMighty <dave@example.com>
2019-02-23 18:45:31 +0000

    Misc changes - reduce h1 font size, reinstate postgres as the database to install the schema to.

--- routes/authorization.php
+++ routes/authorization.php
@@ -7,6 +7,12 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 $app->get("/authorize", function (Request $request, Response $response, array $arguments): Response {
-    //TODO
+
+    # https://authorization-server.com/auth?response_type=code&client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&scope=photos&state=1234zyx
+
+    //TODO: check arguments
+    die(print_r($arguments, true));
+
+
     return $response->withStatus(200)->write("Get /authorize");
 });
